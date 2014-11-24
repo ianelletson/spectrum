ca6496607eff0c06a74143d201aad0b3e9f2d4a7
ianelletson <ianelletson@gmail.com>
2014-11-23 23:49:28 -0600

    done (might have SI problems with -1)

--- src/edu/gustavus/ielletso/Main.java
+++ src/edu/gustavus/ielletso/Main.java
@@ -1,17 +1,18 @@
 package edu.gustavus.ielletso;
 
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.Map;
 import java.util.Scanner;
-import java.util.Stack;
 
 public class Main {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        Stack<String> input = new Stack<String>();
+        LinkedList<String> input = new LinkedList<String>();
         while (true) {
-            input.push(scanner.nextLine());
-            if (input.peek().equals("-1")) {
-                input.pop();
+            input.add(scanner.nextLine());
+            if (input.getLast().equals("-1")) {
                 break;
             }
         }
@@ -21,22 +22,36 @@ public class Main {
         StringBuilder sb = new StringBuilder();
         int caseNum = 0;
         sb.append("Case ").append(++caseNum).append(":\n");
+        Map<String, Node> dict = new HashMap<String, Node>();
         while (!input.isEmpty()) {
-            String[] line = input.pop().split(" ");
+            if (input.peek().equals("-1")) break;
+            String[] line = input.remove().split(" ");
             if (line[0].equals("add")) {
                 if (line.length == 2) {
-                    graph.addNode(new Node(line[1]));
-                    System.out.println(graph.getNodes());
+                    if (!dict.containsKey(line[1]))
+                        dict.put(line[1], new Node(line[1]));
+                    graph.addNode(dict.get(line[1]));
                 }
                 else {
-                    graph.addEdge(new Node(line[1]), new Node(line[2]));
-                    System.out.println(graph.getNodes());
+                    if (!dict.containsKey(line[1]))
+                        dict.put(line[1], new Node(line[1]));
+                    if (!dict.containsKey(line[2]))
+                        dict.put(line[2], new Node(line[2]));
+                    graph.addEdge(dict.get(line[1]), dict.get(line[2]));
                 }
             }
-            if (line[0].equals("connections"))
-                sb.append(graph.getConnections(new Node(line[1])));
-            if (line[0].equals("associated"))
-                sb.append(graph.getAssociation(new Node(line[1]), new Node(line[2])));
+            if (line[0].equals("connections")) {
+                if (!dict.containsKey(line[1]))
+                    dict.put(line[1], new Node(line[1]));
+                sb.append(graph.getConnections(dict.get(line[1])));
+            }
+            if (line[0].equals("associated")) {
+                if (!dict.containsKey(line[1]))
+                    dict.put(line[1], new Node(line[1]));
+                if (!dict.containsKey(line[2]))
+                    dict.put(line[2], new Node(line[2]));
+                sb.append(graph.getAssociation(dict.get(line[1]), dict.get(line[2])));
+            }
         }
         sb.append("----------\n");
         System.out.print(sb);
